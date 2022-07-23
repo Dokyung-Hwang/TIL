e03ccd88b774e7130a30a6c642bd53b4a1cdb936
unknown <hdokyung94@gmail.com>
2022-07-23 21:06:09 +0900

    polymorphism and class

--- src/Java/MethodAndPolymorphism.java
+++ src/Java/MethodAndPolymorphism.java
@@ -1,5 +1,7 @@
 package Java;
 
+
+// Overloading 을 이용한 다형성 예제
 class O {
     public void a(int param){
         System.out.println("숫자 출력");
@@ -12,7 +14,7 @@ class O {
     }
 }
 
-public class Polymorphism {
+public class MethodAndPolymorphism {
     public static void main(String[] args) {
         O o = new O();
         o.a(1);
